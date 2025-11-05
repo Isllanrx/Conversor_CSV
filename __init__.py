0e745396214dc11724a2d79021ad216a8a5978a4
Isllan Toso Pereira <isllan.pereira@globalsys.com.br>
2025-11-05 16:34:37 -0300

    Add __init__.py to define package exports

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .conversor import ConversorCSV
+from .detector_csv import DetectorCSV
+from .interface import AplicacaoConversor
+from .tipos import FormatoConversao
+
+__all__ = ['ConversorCSV', 'DetectorCSV', 'AplicacaoConversor', 'FormatoConversao']
+
